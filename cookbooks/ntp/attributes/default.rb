ed444bf2bed99015bf0bf4592d672dce9238b41b
steven.agg <steven.agg@play.com>
2014-06-10 09:28:22 +0100

    ntp template added

---
+++ cookbooks/ntp/attributes/default.rb
@@ -0,0 +1,5 @@
+default["authorization"]["sudo"]["servers"] = [ "172.17.200.1","172.17.200.2" ]
+
+ 
+ 
+
--- cookbooks/ntp/recipes/default.rb
+++ cookbooks/ntp/recipes/default.rb
@@ -10,13 +10,23 @@ package 'ntp' do
   action :install
 end
 
-cookbook_file "/etc/ntp.conf" do
-  source "lx-ntp.conf"
-  mode "0644"
+#cookbook_file "/etc/ntp.conf" do
+#  source "lx-ntp.conf"
+#  mode "0644"
+#end
+
+template "/etc/ntp.conf" do
+  source "ntp.erb"
+  mode 0644
+  owner "root"
+  group "root"
+  variables({
+     :server_list => node[:authorization][:sudo][:servers],
+  })
 end
 
-  
+ 
+ 
 service 'ntpd' do
   action [ :enable, :start ]
-  subscribes :restart, resources(:template, "/etc/ntp.conf"), :immediately
-end
+  end
